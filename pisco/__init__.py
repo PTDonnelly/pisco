887ae2d5a00dac48fdd6a6172f0a97ebe4890e4e
Padraig Donnelly <padraig2.pd@gmail.com>
2023-06-19 16:43:00 +0200

    Renamed process_iasi_data to scripts to contain all processing and plotting scripts

--- pisco/__init__.py
+++ pisco/__init__.py
@@ -2,4 +2,4 @@ from .configurer import Configurer
 from .correlator import L1C_L2_Correlator
 from .extractor import Extractor
 from .processor import L1CProcessor, L2Processor
-from .process_iasi_data import process_l1c, process_l2, correlate_l1c_l2
+from .scripts import process_l1c, process_l2, correlate_l1c_l2
