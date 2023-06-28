3b144d2518d3696934981f7162bcbb6fe979f7d6
Padraig Donnelly <padraig2.pd@gmail.com>
2023-06-28 16:16:43 +0200

    Update preprocessing.py

--- pisco/preprocessing.py
+++ pisco/preprocessing.py
@@ -380,12 +380,14 @@ class Preprocessor:
         
         # Counter for the valid indices in data
         valid_index = 0
-        
+        byte_offset_increment = 0
+
         for measurement in range(self.metadata.number_of_measurements):
             # Read the value for the current measurement
             value = np.fromfile(self.f, dtype=dtype, count=1, sep='', offset=byte_offset)
+            byte_offset_increment += byte_offset
             if measurement in valid_indices:
-                print(measurement, byte_offset, byte_offset * measurement, self.f.tell() - byte_offset * measurement)
+                print(measurement, byte_offset, byte_offset * measurement, self.f.tell(), self.f.tell() - byte_offset * measurement, byte_offset_increment)
                 input()
                 # Store the value in the data array, handling missing values as NaN
                 data[valid_index] = np.nan if len(value) == 0 else value[0]
