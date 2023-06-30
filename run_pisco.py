7a8f6a6c3dc83884391fac568a987d1e842295d7
Padraig Donnelly <padraig2.pd@gmail.com>
2023-06-30 15:10:17 +0200

    Update run_pisco.py

--- run_pisco.py
+++ run_pisco.py
@@ -1,5 +1,6 @@
+import shutil
+import sys
 import cProfile
-import logging
 
 from pisco import Extractor, flag_data, preprocess_iasi, process_iasi
 
@@ -28,17 +29,25 @@ def main():
             for day in day_range:
                 ex.day = f"{day:02d}"
                 
-                # # Setup logging
-                # logging.basicConfig(filename=f"{ex.config.datapath_out}{ex.year}_{ex.month}_{ex.day}_pisco.log", level=logging.DEBUG)
+                # Setup output logging to save Standard I/O to file
+                logfile = f"{ex.config.datapath_out}pisco.log"
+                with open(logfile, 'w') as f:
+                    # Save the current stdout
+                    _ = sys.stdout
+                    # Redirect stdout to the file
+                    sys.stdout = f
 
-                if (ex.config.L1C) or (ex.config.L2):
-                    valid_indices = flag_data(ex, data_level="l1c")
-                if ex.config.L1C:
-                    preprocess_iasi(ex, valid_indices, data_level="l1c")
-                if ex.config.L2:
-                    preprocess_iasi(ex, valid_indices, data_level="l2")
-                if ex.config.process:
-                    process_iasi(ex)
+                    if (ex.config.L1C) or (ex.config.L2):
+                        valid_indices = flag_data(ex, data_level="l1c")
+                    if ex.config.L1C:
+                        preprocess_iasi(ex, valid_indices, data_level="l1c")
+                    if ex.config.L2:
+                        preprocess_iasi(ex, valid_indices, data_level="l2")
+                    if ex.config.process:
+                        process_iasi(ex)
+                
+                # Move logfile to output directory
+                shutil.move(logfile, ex.datapath_out)
 
 if __name__ == "__main__":
     import time
