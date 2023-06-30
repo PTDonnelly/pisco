15d517363eac36ca5b37535aa080b824700e7d0f
Padraig Donnelly <padraig2.pd@gmail.com>
2023-06-30 17:38:25 +0200

    update

--- pisco/preprocessing.py
+++ pisco/preprocessing.py
@@ -560,8 +560,7 @@ class Preprocessor:
     
     def read_l2_product_fields(self, valid_indices):
         # Retrieve the individual L2 products from the configuration file
-        print(self.metadata.table_of_L2_sections)
-        for product_ID in self.metadata.table_of_L2_sections:
+        for product_ID in list(self.metadata.table_of_L2_sections):
             self.read_record_fields(self.metadata._get_l2_product_record_fields(product_ID), valid_indices)
         
         print(self.data_record_df[["Cloud Phase 1", "Cloud Phase 2", "Cloud Phase 3"]].head())
