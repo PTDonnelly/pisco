5770e3434f17e8df442798e3997b14256f1c747c
Padraig Donnelly <padraig2.pd@gmail.com>
2023-06-30 17:05:33 +0200

    Update preprocessing.py

--- pisco/preprocessing.py
+++ pisco/preprocessing.py
@@ -102,6 +102,8 @@ class Metadata:
         self.channel_IDs = np.fromfile(self.f, dtype='uint32', count=self.number_of_channels)
         self.AVHRR_brilliance = np.fromfile(self.f, dtype='bool', count=1)[0]
         self.number_of_L2_sections = np.fromfile(self.f, dtype='uint16', count=1)[0]
+        print(self.number_of_L2_sections)
+        input()
         if self.number_of_L2_sections:
             print(self.number_of_L2_sections)
             self.table_of_L2_sections = np.fromfile(self.f, dtype='uint32', count=self.number_of_L2_sections)[0]
