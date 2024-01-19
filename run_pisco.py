e1c1047bd6154029c4e5e287a23ef224aeae6aab
Padraig Donnelly <padraig2.pd@gmail.com>
2024-01-19 15:43:05 +0100

    Update run_pisco.py

--- run_pisco.py
+++ run_pisco.py
@@ -98,11 +98,11 @@ def clean_up_files(ex: Extractor, metop: str):
     output_file = f"pisco_{metop}_{ex.year}_{ex.month}_{ex.day}"
 
     # Define source and target paths for the .sh and .log files
-    source_sh = os.path.join(ex.datapath, f"{output_file}.sh")
-    target_sh = os.path.join(ex.datapath, metop, "merged", f"{ex.year}", f"{ex.month}", f"{ex.day}", f"{output_file}.sh")
+    source_sh = os.path.join(ex.config.datapath, f"{output_file}.sh")
+    target_sh = os.path.join(ex.config.datapath, metop, "merged", f"{ex.year}", f"{ex.month}", f"{ex.day}", f"{output_file}.sh")
 
-    source_log = os.path.join(ex.datapath, f"{output_file}.log")
-    target_log = os.path.join(ex.datapath, metop, "merged", f"{ex.year}", f"{ex.month}", f"{ex.day}", f"{output_file}.log")
+    source_log = os.path.join(ex.config.datapath, f"{output_file}.log")
+    target_log = os.path.join(ex.config.datapath, metop, "merged", f"{ex.year}", f"{ex.month}", f"{ex.day}", f"{output_file}.log")
 
     # Function to move a file
     def move_file(source, target):
