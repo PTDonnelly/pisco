ae3783564d4b71e3b8351c0841e5133aed920e6d
Padraig Donnelly <padraig2.pd@gmail.com>
2024-01-19 10:19:23 +0100

    Update __init__.py

--- pisco/__init__.py
+++ pisco/__init__.py
@@ -4,4 +4,3 @@ from .extraction import Extractor
 from .preprocessing import Preprocessor
 from .processing import Processor
 from .plotting import Plotter, Spectrum, Geographic
-from .scripts import flag_data, preprocess_iasi, process_iasi
