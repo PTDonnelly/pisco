bbef407333fa992407c3fbefbf8620ad3bf10a24
Padraig Donnelly <padraig2.pd@gmail.com>
2024-01-29 13:49:58 +0100

    Update processing.py

--- pisco/processing.py
+++ pisco/processing.py
@@ -98,8 +98,7 @@ class Processor:
         
         # Merge two DataFrames based on latitude, longitude and datetime,
         # rows from df_l1c that do not have a corresponding row in df_l2 are dropped.
-        merged_df = pd.merge(self.df_l1c, self.df_l2, on=['Latitude', 'Longitude', 'Datetime'], how='inner')
-        print(merged_df[["SatelliteZenithAngle_x", "SatelliteZenithAngle_y"]].head())
+        merged_df = pd.merge(self.df_l1c, self.df_l2, on=["Datetime", "Latitude", 'Longitude', "SatelliteZenithAngle"], how='inner')
 
         # Keep only columns containing variables present in reduced_fields and spectral channels
         reduced_fields = self._get_reduced_fields()
