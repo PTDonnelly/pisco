65b833b46850c6cfff83eaf3a306a80d6d1ca261
Padraig Donnelly <padraig2.pd@gmail.com>
2024-02-01 16:49:35 +0100

    Update scripts.py

--- pisco/scripts.py
+++ pisco/scripts.py
@@ -597,7 +597,7 @@ def plot_pisco():
     """
     # The path to the directory that contains the data files
     # datapath = "D:\\Data\\iasi\\"
-    datapath = "/data/pdonnelly/iasi/metopb/"
+    datapath = "/data/pdonnelly/iasi/metopb_window/"
 
     # Define temporal range to plot
     target_year = [2013, 2014, 2015, 2016, 2017, 2018, 2019]
