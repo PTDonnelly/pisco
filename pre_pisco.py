54f5065bc8fb9ed058d8488ab9e062773fd914c8
Padraig Donnelly <padraig2.pd@gmail.com>
2024-02-09 16:20:08 +0100

    Update pre_pisco.py

--- pre_pisco.py
+++ pre_pisco.py
@@ -1,4 +1,3 @@
-import os
 import subprocess
 from pisco import Extractor
 
@@ -56,8 +55,8 @@ def main():
                 # Set execute permissions on the script
                 subprocess.run(["chmod", "+x", script])
 
-                # # Submit the batch script to SLURM using sbatch
-                # subprocess.run(["sbatch", script])
+                # Submit the batch script to SLURM using sbatch
+                subprocess.run(["sbatch", script])
 
 if __name__ == "__main__":
     main()
