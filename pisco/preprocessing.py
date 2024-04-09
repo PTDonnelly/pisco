2a0f89fe394b18bf01107c88d2618fa3bff21ab4
Padraig Donnelly <padraig2.pd@gmail.com>
2024-04-09 11:31:39 +0200

    Update preprocessing.py

--- pisco/preprocessing.py
+++ pisco/preprocessing.py
@@ -157,7 +157,10 @@ class Preprocessor:
             # Concatenate 'Date' and 'Time' columns into a single 'Datetime' string
             self.df['Datetime'] = self.df['Date'] + self.df['Time']
             # Convert 'Datetime' string to a datetime object
-            self.df['Datetime'] = pd.to_datetime(self.df['Datetime'], format='%Y%m%d%H%M')
+            self.df['Datetime'] = pd.to_datetime(self.df['Datetime'], format='%Y%m%d%H%M%S')
+            # Round down to the nearest minute
+            self.df['Datetime'] = self.df['Datetime'].dt.floor('T')
+
 
             # Drop individual Date and Time columns
             self.df.drop(columns=['Date', 'Time'], inplace=True)
